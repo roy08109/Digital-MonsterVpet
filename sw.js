93aff74c479b9e9c5724ca7dfd38d926787883b6
roy08109 <roy08109@gmail.com>
2026-07-24 17:12:00 +0800

    0.4

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "digimon-vpet-v1";
+
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      const fetched = fetch(e.request).then((res) => {
+        if(res && res.ok){
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || fetched;
+    })
+  );
+});
