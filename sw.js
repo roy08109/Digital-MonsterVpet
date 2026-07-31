f72f88aff9bd834f9c747232b4d07311fbc6070b
roy08109 <roy08109@gmail.com>
2026-07-31 17:09:31 +0800

    0.99

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "digimon-vpet-v1";
+const CACHE_NAME = "digimon-vpet-v16";
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (e) => {
